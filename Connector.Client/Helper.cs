8df02ffd5f54829c71953f6098daaa221f1f05c0
Sajid <moinccesri@gmail.com>
2023-10-02 11:15:54 +0300

    DB

---
+++ Connector.Client/Helper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Connector.Client
+{
+    public class Helper
+    {
+        
+    }
+    /// <summary>
+    /// HTTP method to use when making requests
+    /// </summary>
+    public enum Method
+    {
+        Get, Post, Put, Delete, Head, Options,
+
+    }
+}
