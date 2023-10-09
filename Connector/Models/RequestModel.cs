2c681d1045eaad22cc4de6a631e272a832e1a65a
Sajid <moinccesri@gmail.com>
2023-10-09 11:01:35 +0300

    Post request

--- Connector/Models/RequestModel.cs
+++ Connector/Models/RequestModel.cs
@@ -8,8 +8,16 @@ namespace Connector.Models
         public Method Method { get; set; }
         public string Uri { get; set; }
         public Page Page { get; set; }
+
+        public string Body { get; set; }
+        public RequestBodyType RequestBodyType { get; set; }
         public List<KeyValueParameter> Headers { get; set; }
         public List<KeyValueParameter> Parameters { get; set; }
+    }
+
+    public class RequestBody
+    {
+        public string Body { get; set; }
     }
 
     public class KeyValueParameter
